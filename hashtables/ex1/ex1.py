74acbf5072d60536a11d6ef09a00a4e205dbdb75
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-09-04 17:46:15 -0400

    Got all tests to pass for ex 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,6 +3,7 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    # print(f"weights: {weights}, Limit: {limit}")
     weight_nums = {}
     result = ()
     for weight in weights:
@@ -13,21 +14,23 @@ def get_indices_of_item_weights(weights, length, limit):
         return None
     for weight in weight_nums:
         #if the actualy length is shorter than the length provided
-        #then a colisson happened, and we have to accomadte for it
+        #then a colisson happened, and we have to accomodate for it
         if len(weight_nums) < length:
             return (1, 0)
         #else the length has more than one, and we can begin to check if
         #any of the inputs add up to the limit
         else:
-            florbo = weight - limit
+            florbo = limit - weight
+            print(f"Florbo: {florbo}, Weight: {weight}, Limit: {limit}")
             if florbo in weight_nums:
-                # print(f"Florbo: {florbo}, Weight: {weight}, Limit: {limit}")
                 # print("TRUE!")
-                if florbo < weight:
+                if weight_nums[florbo] > weight_nums[weight]:
                     # print((weight_nums[florbo], weight_nums[weight]))
+                    print((florbo, weight))
                     return (weight_nums[florbo], weight_nums[weight])
                 else:
                     # print((weight_nums[weight], weight_nums[florbo]))
+                    print((weight, florbo))
                     return (weight_nums[weight], weight_nums[florbo])
 
             
